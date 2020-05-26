14f09cdb20a7c1e3987c9c633048f533f87340c6
zTreeAPI <hunter.z@263.net>
2020-05-26 12:44:27 +0800

    所见即所得模式下 支持 preview.transform ，便于统一处理规则（尤其是图片路径）

--- src/ts/wysiwyg/renderDomByMd.ts
+++ src/ts/wysiwyg/renderDomByMd.ts
@@ -3,7 +3,13 @@ import {afterRenderEvent} from "./afterRenderEvent";
 
 export const renderDomByMd = (vditor: IVditor, md: string, enableInput = true) => {
     const editorElement = vditor.wysiwyg.element;
-    editorElement.innerHTML = vditor.lute.Md2VditorDOM(md);
+    // editorElement.innerHTML = vditor.lute.Md2VditorDOM(md);
+    // 2020-05-26 所见即所得模式下 支持 preview.transform ，便于统一处理规则（尤其是图片路径）
+    let html = vditor.lute.Md2VditorDOM(md);
+     if (vditor.options.preview.transform) {
+        html = vditor.options.preview.transform(html);
+    }
+    editorElement.innerHTML = html;
 
     editorElement.querySelectorAll(".vditor-wysiwyg__preview[data-render='2']").forEach((item: HTMLElement) => {
         processCodeRender(item, vditor);
