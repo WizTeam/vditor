c617eb02663b72cd7674e178dc83117bb200d0a6
zTreeAPI <hunter.z@263.net>
2020-06-19 11:30:46 +0800

    增加 demo 的测试代码

--- demo/index.js
+++ demo/index.js
@@ -49,6 +49,7 @@ if (window.innerWidth < 768) {
 }
 
 window.vditor = new Vditor('vditor', {
+  cdn: 'https://cdn.jsdelivr.net/npm/vditor@3.2.12',
   mode: 'ir',
   toolbar,
   height: window.innerHeight + 100,
@@ -61,7 +62,8 @@ window.vditor = new Vditor('vditor', {
       toc: true,
     },
     transform: function(html) {
-      console.log(html);
+      html = html.replace(/\n<\//ig, '</');
+      // console.log(html);
       return html;
     }
   },
